bf9174411b52c3f9bd1def83b836a3f29bb62976
Emily Easteal <emily@easteal.dev>
2021-07-05 21:29:05 +0100

    first commit

---
+++ braille.py
@@ -0,0 +1,49 @@
+KEYS = ('L', 'f', 'd', 's', 'j', 'k', 'l', 'B', 'S')
+
+IMPLICIT_HYPHEN_KEYS = KEYS
+
+KEYMAPS = {
+        'Gemini PR': {
+            'L': ('S1-', 'S2-'),
+            'f': ('H-', 'R-'),
+            'd': ('P-', 'W-'),
+            's': ('T-', 'K-'),
+            'j': ('-F', '-R'),
+            'k': ('-P', '-B'),
+            'l': ('-L', '-G'),
+            'B': ('-T', '-S'),
+            'S': ('A-', 'O-', '-E', '-U'),
+        },
+        'Keyboard': {
+            'L': ('q', 'a'),
+            'f': ('r', 'f'),
+            'd': ('e', 'd'),
+            's': ('w', 's'),
+            'j': ('u', 'j'),
+            'k': ('i', 'k'),
+            'l': ('o', 'l'),
+            'B': ('p', ';'),
+            'S': ('space', 'c', 'v', 'n', 'm'),
+            'arpeggiate': ('g', 'h'),
+            'no-op': ('x', 'b', ','),
+        },
+}
+
+UNDO_STROKE_STENO = 'B'
+
+NUMBERS = {}
+SUFFIX_KEYS = ()
+NUMBER_KEY = None
+ORTHOGRAPHY_RULES = []
+ORTHOGRAPHY_WORDLIST = None
+ORTHOGRAPHY_RULES_ALIASES = {}
+
+DICTIONARIES_ROOT = 'asset:dictionaries'
+DEFAULT_DICTIONARIES = (
+        'braille-alphabet.json',
+        'braille-formatting.json',
+        'braille-symbols.json',
+        'braille-abreviations.json',
+        'braille-contractions.json',
+        'braille-signs.json',
+)
